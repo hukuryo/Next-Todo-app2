dc1b01c2dd41107827430f58e87e8950bcf1797c
hukuryo <ryohei.1998.05.09@gmail.com>
2023-06-19 21:06:00 +0900

    リストコンポーネント作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 "use client";
 import React, { MouseEventHandler, useCallback, useRef, useState } from 'react';
 import Form from "../app/components/Form";
+import CompleteList from "../app/components/CompleteLists";
+import CompletedList from "../app/components/CompletedLists";
 
 export default function Home() {
   return (
     <main className="min-h-screen items-center justify-between p-24">
       <Form/>
+      <CompleteList/>
+      <CompletedList/>
   </main>
   )
 }
