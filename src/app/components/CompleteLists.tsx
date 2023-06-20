470795e75a852b0d9554aecd38e87311b1e5bc0b
hukuryo <ryohei.1998.05.09@gmail.com>
2023-06-20 21:30:30 +0900

    リストにダミーデータを表示できるようになりました

--- src/app/components/CompleteLists.tsx
+++ src/app/components/CompleteLists.tsx
@@ -1,14 +1,35 @@
-"use client";
-import React, { MouseEventHandler, useCallback, useRef, useState } from 'react';
+import React, { useState } from 'react';
 
+interface Todo {
+    task: string;
+    isCompleted: boolean;
+}
 
 export default function CompleteList() {
+    const initialState: Todo[] = [
+        {
+        task: 'Learn vue.js',
+        isCompleted: false,
+        },
+        {
+        task: 'Learn React Hook',
+        isCompleted: false,
+        },
+        {
+        task: 'Learn Gatsby.js',
+        isCompleted: false,
+        }
+    ];
+    const [todos, setTodos] = useState(initialState);
+
     return (
         <ul className="mt-6 space-y-4">
-            <li className="flex items-center justify-between bg-white border border-gray-300 px-4 py-2 rounded-md">
-                <span></span>
-                <button className="text-red-600 hover:text-red-800">削除</button>
+        {todos.map((todo: Todo, index: number) => (
+            <li key={index} className="flex items-center justify-between bg-white border border-gray-300 px-4 py-2 rounded-md">
+            <span>{todo.task}</span>
+            <button className="text-red-600 hover:text-red-800">削除</button>
             </li>
+        ))}
         </ul>
-    )
+    );
 }
--- src/app/components/Form.tsx
+++ src/app/components/Form.tsx
@@ -1,47 +1,24 @@
-import React, { useState } from 'react';
-
-type CompleteListProps = {
-    textList: string[];
-};
-
-const CompleteList: React.FC<CompleteListProps> = ({ textList }) => {
-    return (
-        <ul>
-        {textList.map((text, index) => (
-            <li key={index}>{text}</li>
-        ))}
-        </ul>
-    );
-};
+"use client";
+import React, { MouseEventHandler, useCallback, useRef, useState } from 'react';
+import CompleteList from "../components/CompleteLists";
 
 export default function Form() {
     const [todoText, setTodoText] = useState<string[]>([]);
-    const setTodoTextInput = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        const input = e.currentTarget.elements.namedItem('todoInput') as HTMLInputElement;
-        const newTodoText = input.value.trim();
-        if (newTodoText !== '') {
-        setTodoText((prevText) => [...prevText, newTodoText]);
-        input.value = '';
-        }
+    const setTodoTextInput = (e: any) => {
+        setTodoText(e.target.value);
+    }
+    type TodoListProps = {
+        textList: string[];
     };
-
     return (
         <div className="px-4 py-4 sm:p-6">
-        <div className="mb-4">
-            <form onSubmit={setTodoTextInput}>
-            <input
-                type="text"
-                id="todoInput"
-                className="w-full border border-gray-300 px-3 py-2 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
-                placeholder="タスクを入力してください"
-            />
-            <button type="submit" className="bg-indigo-600 text-white px-4 py-2 rounded-md">
-                追加する
-            </button>
-            </form>
+            <div className="mb-4">
+                <form onSubmit={setTodoTextInput}>
+                    <input type="text" className="w-full border border-gray-300 px-3 py-2 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500" placeholder="タスクを入力してください"/>
+                    <button className="bg-indigo-600 text-white px-4 py-2 rounded-md">追加する</button>
+                </form>
+            </div>
+            <CompleteList/>
         </div>
-        <CompleteList textList={todoText} />
-        </div>
-    );
+    )
 }
