7805c730ab2cede73a4c48dab10673c7787d7ee1
hukuryo <ryohei.1998.05.09@gmail.com>
2023-06-20 19:59:22 +0900

    デザインをいい感じにしました

--- src/app/components/CompleteLists.tsx
+++ src/app/components/CompleteLists.tsx
@@ -4,10 +4,11 @@ import React, { MouseEventHandler, useCallback, useRef, useState } from 'react';
 
 export default function CompleteList() {
     return (
-        <div className=''>
-        <ul>
-            <li>test2</li>
-            </ul>
-        </div>
+        <ul className="mt-6 space-y-4">
+            <li className="flex items-center justify-between bg-white border border-gray-300 px-4 py-2 rounded-md">
+                <span></span>
+                <button className="text-red-600 hover:text-red-800">削除</button>
+            </li>
+        </ul>
     )
 }
