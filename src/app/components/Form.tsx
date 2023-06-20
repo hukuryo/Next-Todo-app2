28c0623fb3c1c3979a05abde590d4d6e3abbd8e4
hukuryo <ryohei.1998.05.09@gmail.com>
2023-06-20 21:04:51 +0900

    フォームで値を取得できるようにしました

--- src/app/components/Form.tsx
+++ src/app/components/Form.tsx
@@ -1,15 +1,47 @@
-"use client";
-import React, { MouseEventHandler, useCallback, useRef, useState } from 'react';
-import CompleteList from "../components/CompleteLists";
+import React, { useState } from 'react';
+
+type CompleteListProps = {
+    textList: string[];
+};
+
+const CompleteList: React.FC<CompleteListProps> = ({ textList }) => {
+    return (
+        <ul>
+        {textList.map((text, index) => (
+            <li key={index}>{text}</li>
+        ))}
+        </ul>
+    );
+};
 
 export default function Form() {
+    const [todoText, setTodoText] = useState<string[]>([]);
+    const setTodoTextInput = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const input = e.currentTarget.elements.namedItem('todoInput') as HTMLInputElement;
+        const newTodoText = input.value.trim();
+        if (newTodoText !== '') {
+        setTodoText((prevText) => [...prevText, newTodoText]);
+        input.value = '';
+        }
+    };
+
     return (
         <div className="px-4 py-4 sm:p-6">
-            <div className="mb-4">
-                <input type="text" className="w-full border border-gray-300 px-3 py-2 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500" placeholder="タスクを入力してください"/>
-            </div>
-            <button className="bg-indigo-600 text-white px-4 py-2 rounded-md">追加する</button>
-            <CompleteList/>
+        <div className="mb-4">
+            <form onSubmit={setTodoTextInput}>
+            <input
+                type="text"
+                id="todoInput"
+                className="w-full border border-gray-300 px-3 py-2 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                placeholder="タスクを入力してください"
+            />
+            <button type="submit" className="bg-indigo-600 text-white px-4 py-2 rounded-md">
+                追加する
+            </button>
+            </form>
         </div>
-    )
+        <CompleteList textList={todoText} />
+        </div>
+    );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,13 @@
 "use client";
 import React, { MouseEventHandler, useCallback, useRef, useState } from 'react';
 import Form from "../app/components/Form";
+import Title from "../app/components/Title";
 
 export default function Home() {
   return (
-    // <main className="min-h-screen items-center justify-between p-24">
-    //   <Form/>
-    
-    //   <CompletedList/>
-    // </main>
     <main className="min-h-screen bg-gray-100 py-10 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md mx-auto bg-white rounded-lg shadow-md overflow-hidden">
-        <div className="px-4 py-5 sm:px-6 bg-indigo-600">
-          <h1 className="text-lg font-semibold text-white">TODOアプリ</h1>
-        </div>
+        <Title/>
         <Form/>
       </div>
     </main>
