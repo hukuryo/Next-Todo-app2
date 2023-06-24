5687263c4e9f41af367240650a4565e42d5e5d23
hukuryo <ryohei.1998.05.09@gmail.com>
2023-06-24 18:06:43 +0900

    削除機能を作成

--- src/app/components/CompleteLists.tsx
+++ src/app/components/CompleteLists.tsx
@@ -5,33 +5,18 @@ interface Todo {
     isCompleted: boolean;
 }
 
-    interface CompleteListProps {
-        textList: Todo[];
-    }
+interface CompleteListProps {
+    textList: Todo[];
+    onDelete: (index: number) => void;
+}
 
-export default function CompleteList({ textList }: CompleteListProps) {
-    const initialState: Todo[] = [
-        {
-        task: 'Learn vue.js',
-        isCompleted: false,
-        },
-        {
-        task: 'Learn React Hook',
-        isCompleted: false,
-        },
-        {
-        task: 'Learn Gatsby.js',
-        isCompleted: false,
-        },
-    ];
-    const [todos, setTodos] = useState<Todo[]>(initialState);
-
+export default function CompleteList({ textList, onDelete }: CompleteListProps) {
     return (
         <ul className="mt-6 space-y-4">
         {textList.map((todo: Todo, index: number) => (
             <li key={index} className="flex items-center justify-between bg-white border border-gray-300 px-4 py-2 rounded-md">
-                <span>{todo.task}</span>
-                <button className="text-red-600 hover:text-red-800">削除</button>
+            <span>{todo.task}</span>
+            <button className="text-red-600 hover:text-red-800" onClick={() => onDelete(index)}>削除</button>
             </li>
         ))}
         </ul>
