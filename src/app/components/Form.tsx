3329b3ca8b98733ddbcdbf977bf41856d2a20c68
hukuryo <ryohei.1998.05.09@gmail.com>
2023-06-25 10:31:36 +0900

    送信後にフォームの中身を空にできるようにした

--- src/app/components/Form.tsx
+++ src/app/components/Form.tsx
@@ -25,6 +25,7 @@ export default function Form() {
             return;
         }
         setTextList(prevTextList => [...prevTextList, newTodo]);
+        setTodoText('');
     };
 
     const handleDelete = (index: number) => {
