97f937dda2942c11e3390b907b0e4149ac5923d9
hukuryo <ryohei.1998.05.09@gmail.com>
2023-06-25 09:57:27 +0900

    使わないコンポーネントファイルを削除

--- src/app/components/CompleteLists.tsx
+++ src/app/components/CompleteLists.tsx
@@ -13,12 +13,12 @@ interface CompleteListProps {
 export default function CompleteList({ textList, onDelete }: CompleteListProps) {
     return (
         <ul className="mt-6 space-y-4">
-        {textList.map((todo: Todo, index: number) => (
-            <li key={index} className="flex items-center justify-between bg-white border border-gray-300 px-4 py-2 rounded-md">
-            <span>{todo.task}</span>
-            <button className="text-red-600 hover:text-red-800" onClick={() => onDelete(index)}>削除</button>
-            </li>
-        ))}
+            {textList.map((todo: Todo, index: number) => (
+                <li key={index} className="flex items-center justify-between bg-white border border-gray-300 px-4 py-2 rounded-md">
+                <span>{todo.task}</span>
+                <button className="text-red-600 hover:text-red-800" onClick={() => onDelete(index)}>削除</button>
+                </li>
+            ))}
         </ul>
     );
 }
