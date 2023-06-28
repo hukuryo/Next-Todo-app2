0910ec352420948e1cacb4e941fe9a6b9955e84d
hukuryo <ryohei.1998.05.09@gmail.com>
2023-06-28 21:10:06 +0900

    モーダルを表示を大きくした

--- src/app/components/CompleteLists.tsx
+++ src/app/components/CompleteLists.tsx
@@ -39,25 +39,13 @@ export default function CompleteList({ textList, onDelete }: CompleteListProps) 
             </ul>
 
             {modalOpen && selectedTodoIndex !== -1 && (
-                // <EditModal
-                //     todo={textList[selectedTodoIndex]}
-                //     onSave={(editedTodo: Todo) => {
-                //         setModalOpen(true);
-                //     }}
-                //     onCancel={() => setModalOpen(false)}
-                // />
-                <div>
-                    {modalOpen && (
-                        <div className="fixed inset-0 flex items-center justify-center bg-gray-500 bg-opacity-50">
-                        <div className="bg-white p-4">
-                            <h3>Edit Todo</h3>
-                            <button className="bg-indigo-600 text-white px-4 py-2 rounded-md" onClick={() => setModalOpen(false)}>
-                            Close
-                            </button>
-                        </div>
-                        </div>
-                    )}
-                </div>
+                <EditModal
+                    todo={textList[selectedTodoIndex]}
+                    onSave={(editedTodo: Todo) => {
+                        setModalOpen(true);
+                    }}
+                    onCancel={() => setModalOpen(false)}
+                />
             )}
         </div>
     );
--- src/app/components/EditModal.tsx
+++ src/app/components/EditModal.tsx
@@ -18,13 +18,13 @@ export default function EditModal({ todo, onSave, onCancel }: EditModalProps) {
     return (
         <div>
             {modalOpen && (
-                <div className="fixed inset-0 flex items-center justify-center bg-gray-500 bg-opacity-50">
-                <div className="bg-white p-4">
-                    <h3>Edit Todo</h3>
-                    <button className="bg-indigo-600 text-white px-4 py-2 rounded-md" onClick={() => setModalOpen(false)}>
-                    Close
-                    </button>
-                </div>
+                <div className="fixed inset-0 flex items-center justify-center bg-gray-500">
+                <div className="bg-white p-4 max-w-screen-lg mx-auto">
+                    <h3 className="p-5">Edit Todo</h3>
+                        <button className="bg-indigo-600 text-white px-4 py-2 rounded-md" onClick={() => setModalOpen(false)}>
+                            Close
+                        </button>
+                    </div>
                 </div>
             )}
         </div>
