bade2e4d4a697cf828e4b2eec64b45f9b9008b5c
hukuryo <ryohei.1998.05.09@gmail.com>
2023-06-28 21:22:49 +0900

    NextAuth.jsをインストール

--- src/app/components/CompleteLists.tsx
+++ src/app/components/CompleteLists.tsx
@@ -39,13 +39,25 @@ export default function CompleteList({ textList, onDelete }: CompleteListProps) 
             </ul>
 
             {modalOpen && selectedTodoIndex !== -1 && (
-                <EditModal
-                    todo={textList[selectedTodoIndex]}
-                    onSave={(editedTodo: Todo) => {
-                        setModalOpen(true);
-                    }}
-                    onCancel={() => setModalOpen(false)}
-                />
+                // <EditModal
+                //     todo={textList[selectedTodoIndex]}
+                //     onSave={(editedTodo: Todo) => {
+                //         setModalOpen(true);
+                //     }}
+                //     onCancel={() => setModalOpen(false)}
+                // />
+                <div>
+                    {modalOpen && (
+                        <div className="fixed inset-0 flex items-center justify-center bg-gray-500">
+                        <div className="bg-white p-4 max-w-screen-lg mx-auto">
+                            <h3 className="p-5">Edit Todo</h3>
+                                <button className="bg-indigo-600 text-white px-4 py-2 rounded-md" onClick={() => setModalOpen(false)}>
+                                    Close
+                                </button>
+                            </div>
+                        </div>
+                    )}
+                </div>
             )}
         </div>
     );
